45d4b47edb3b7c4bd7db21ea73e2529d926fb340
Charles <upsilonalcubo@hotmail.com>
2019-05-27 14:53:47 -0500

    Initialize repository

---
+++ index.js
@@ -0,0 +1,61 @@
+// Reverses a string.
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Returns true for a palindroem, false otherwise
+function palindrome(string) {
+  let processedContent = string.toLowerCase();
+  return processedContent === reverse(processedContent);
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns the lower-case version of the content.
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true for a palindroem, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
+  // EXERCISE
+  this.louder = function() {
+    let processedContent = this.content.toUpperCase();
+    return processedContent;
+  }
+}
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palidrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
+
+// EXERCISES
+function emailParts(email) {
+  let cutter = /\w+/g;
+  let processedContent = email.match(cutter).slice(0, -1);
+  return `Username: ${processedContent[0]}, Domain: ${processedContent[1]}`;
+}
